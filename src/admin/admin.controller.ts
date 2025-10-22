d411fe765c5340b719866606219b208f80309d63
duynm <duynm.nkaitech@gmail.com>
2025-10-22 11:18:57 +0700

    commit

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -3,14 +3,13 @@ import { FirebaseAuthGuard } from '../common/guards/firebase-auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
 import { Roles } from '../auth/roles.decorator';
 import { AdminService } from './admin.service';
+import { FirebaseSessionGuard } from 'src/common/guards/firebase-session.guard';
 
 @Controller('admin')
 @Roles('admin')
-@UseGuards(RolesGuard)
+@UseGuards(RolesGuard, FirebaseSessionGuard)
 export class AdminController {
     constructor(private readonly adminService: AdminService) { }
-
-    @UseGuards(FirebaseAuthGuard)
     @Get('users')
     async getAllUsers(@Req() req) {
         return this.adminService.getAllUsers();
